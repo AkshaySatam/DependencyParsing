308e0b20ef92abb693f6a67ddf08a0991b0aa6b0
Akshay <akshaysatam10@gmail.com>
2018-04-05 22:21:40 -0400

    Done with features

---
+++ DependencyParser.py
@@ -0,0 +1,643 @@
+import collections
+import tensorflow as tf
+import numpy as np
+import pickle
+import math
+from progressbar import ProgressBar
+
+from DependencyTree import DependencyTree
+from ParsingSystem import ParsingSystem
+from Configuration import Configuration
+import Config
+import Util
+
+"""
+This script defines a transition-based dependency parser which makes
+use of a classifier powered by a neural network. The neural network
+accepts distributed representation inputs: dense, continuous
+representations of words, their part of speech tags, and the labels
+which connect words in a partial dependency parse.
+
+This is an implementation of the method described in
+
+Danqi Chen and Christopher Manning. A Fast and Accurate Dependency Parser Using Neural Networks. In EMNLP 2014.
+
+Author: Danqi Chen, Jon Gauthier
+Modified by: Heeyoung Kwon (2017)
+Modified by: Jun S. Kang (2018 Mar)
+"""
+
+
+class DependencyParserModel(object):
+
+    def __init__(self, graph, embedding_array, Config):
+
+        self.build_graph(graph, embedding_array, Config)
+
+    def build_graph(self, graph, embedding_array, Config):
+        """
+
+        :param graph:
+        :param embedding_array:
+        :param Config:
+        :return:
+        """
+
+        with graph.as_default():
+            self.embeddings = tf.Variable(embedding_array, dtype=tf.float32)
+
+            """
+            ===================================================================
+
+            Define the computational graph with necessary variables.
+            
+            1) You may need placeholders of:
+                - Many parameters are defined at Config: batch_size, n_Tokens, etc
+                - # of transitions can be get by calling parsing_system.numTransitions()
+                
+            
+            ...self.train_inputs = 
+            self.train_labels = 
+            self.test_inputs =
+            
+                
+            2) Call forward_pass and get predictions
+            
+            ...
+            self.prediction = self.forward_pass(embed, weights_input, biases_input, weights_output)
+
+
+            3) Implement the loss function described in the paper
+             - lambda is defined at Config.lam
+            
+            ...
+            self.loss =
+            
+            ===================================================================
+            """
+
+
+            #Defining the placeholders. Need to check on the sizes
+            self.train_inputs = tf.placeholder(tf.int32, shape=[embedding_size])
+            self.train_labels = tf.placeholder(tf.int32, shape=[embedding_size, 1])
+            self.test_inputs = tf.placeholder(tf.int32, shape=[embedding_size])
+
+            #Defining the parameters of the model as variables
+            weights_input = tf.Variable()
+            bias_input = tf.Variable()
+            weights_output = tf.Variable()
+
+
+            optimizer = tf.train.GradientDescentOptimizer(Config.learning_rate)
+            grads = optimizer.compute_gradients(self.loss)
+            clipped_grads = [(tf.clip_by_norm(grad, 5), var) for grad, var in grads]
+            self.app = optimizer.apply_gradients(clipped_grads)
+
+            # For test data, we only need to get its prediction
+            test_embed = tf.nn.embedding_lookup(self.embeddings, self.test_inputs)
+            test_embed = tf.reshape(test_embed, [1, -1])
+            self.test_pred = self.forward_pass(test_embed, weights_input, biases_input, weights_output)
+
+            # intializer
+            self.init = tf.global_variables_initializer()
+
+    def train(self, sess, num_steps):
+        """
+
+        :param sess:
+        :param num_steps:
+        :return:
+        """
+        self.init.run()
+        print "Initailized"
+
+        average_loss = 0
+        for step in range(num_steps):
+            start = (step * Config.batch_size) % len(trainFeats)
+            end = ((step + 1) * Config.batch_size) % len(trainFeats)
+            if end < start:
+                start -= end
+                end = len(trainFeats)
+            batch_inputs, batch_labels = trainFeats[start:end], trainLabels[start:end]
+
+            feed_dict = {self.train_inputs: batch_inputs, self.train_labels: batch_labels}
+
+            _, loss_val = sess.run([self.app, self.loss], feed_dict=feed_dict)
+            average_loss += loss_val
+
+            if step % Config.display_step == 0:
+                if step > 0:
+                    average_loss /= Config.display_step
+                print "Average loss at step ", step, ": ", average_loss
+                average_loss = 0
+            if step % Config.validation_step == 0 and step != 0:
+                print "\nTesting on dev set at step ", step
+                predTrees = []
+                for sent in devSents:
+                    numTrans = parsing_system.numTransitions()
+
+                    c = parsing_system.initialConfiguration(sent)
+                    while not parsing_system.isTerminal(c):
+                        feat = getFeatures(c)
+                        pred = sess.run(self.test_pred, feed_dict={self.test_inputs: feat})
+
+                        optScore = -float('inf')
+                        optTrans = ""
+
+                        for j in range(numTrans):
+                            if pred[0, j] > optScore and parsing_system.canApply(c, parsing_system.transitions[j]):
+                                optScore = pred[0, j]
+                                optTrans = parsing_system.transitions[j]
+
+                        c = parsing_system.apply(c, optTrans)
+
+                    predTrees.append(c.tree)
+                result = parsing_system.evaluate(devSents, predTrees, devTrees)
+                print result
+
+        print "Train Finished."
+
+    def evaluate(self, sess, testSents):
+        """
+
+        :param sess:
+        :return:
+        """
+
+        print "Starting to predict on test set"
+        predTrees = []
+        for sent in testSents:
+            numTrans = parsing_system.numTransitions()
+
+            c = parsing_system.initialConfiguration(sent)
+            while not parsing_system.isTerminal(c):
+                # feat = getFeatureArray(c)
+                feat = getFeatures(c)
+                pred = sess.run(self.test_pred, feed_dict={self.test_inputs: feat})
+
+                optScore = -float('inf')
+                optTrans = ""
+
+                for j in range(numTrans):
+                    if pred[0, j] > optScore and parsing_system.canApply(c, parsing_system.transitions[j]):
+                        optScore = pred[0, j]
+                        optTrans = parsing_system.transitions[j]
+
+                c = parsing_system.apply(c, optTrans)
+
+            predTrees.append(c.tree)
+        print "Saved the test results."
+        Util.writeConll('result_test.conll', testSents, predTrees)
+
+
+    def forward_pass(self, embed, weights_input, biases_input, weights_output):
+        """
+
+        :param embed:
+        :param weights:
+        :param biases:
+        :return:
+        """
+        """
+        =======================================================
+
+        Implement the forwrad pass described in
+        "A Fast and Accurate Dependency Parser using Neural Networks"(2014)
+
+        =======================================================
+        """
+        
+
+
+
+
+def genDictionaries(sents, trees):
+    word = []
+    pos = []
+    label = []
+    for s in sents:
+        for token in s:
+            word.append(token['word'])
+            pos.append(token['POS'])
+
+    rootLabel = None
+    for tree in trees:
+        for k in range(1, tree.n + 1):
+            if tree.getHead(k) == 0:
+                rootLabel = tree.getLabel(k)
+            else:
+                label.append(tree.getLabel(k))
+
+    if rootLabel in label:
+        label.remove(rootLabel)
+
+    index = 0
+    wordCount = [Config.UNKNOWN, Config.NULL, Config.ROOT]
+    wordCount.extend(collections.Counter(word))
+    for word in wordCount:
+        wordDict[word] = index
+        index += 1
+
+    posCount = [Config.UNKNOWN, Config.NULL, Config.ROOT]
+    posCount.extend(collections.Counter(pos))
+    for pos in posCount:
+        posDict[pos] = index
+        index += 1
+
+    labelCount = [Config.NULL, rootLabel]
+    labelCount.extend(collections.Counter(label))
+    for label in labelCount:
+        labelDict[label] = index
+        index += 1
+
+    return wordDict, posDict, labelDict
+
+
+def getWordID(s):
+    if s in wordDict:
+        return wordDict[s]
+    else:
+        return wordDict[Config.UNKNOWN]
+
+
+def getPosID(s):
+    if s in posDict:
+        return posDict[s]
+    else:
+        return posDict[Config.UNKNOWN]
+
+
+def getLabelID(s):
+    if s in labelDict:
+        return labelDict[s]
+    else:
+        return labelDict[Config.UNKNOWN]
+
+def addElement(elementList,s1Word,i):
+    elementList[i] = s1Word
+
+def getFeatures(c):
+
+    """
+    =================================================================
+
+    Implement feature extraction described in
+    "A Fast and Accurate Dependency Parser using Neural Networks"(2014)
+
+    =================================================================
+    """
+    elementList =[]
+    i=0
+
+    s1 = c.getStack(0)
+    elementList.append(s1)
+    s2 = c.getStack(1)
+    elementList.append(s2)
+    s3 = c.getStack(2)
+    elementList.append(s3)
+
+    b1 = c.getBuffer(0)
+    elementList.append(b1)
+    b2 = c.getBuffer(1)
+    elementList.append(b2)
+    b3 = c.getBuffer(2)
+    elementList.append(b3)
+
+
+    # s1 data
+    s1_lc1 = c.getLeftChild(s1,1)
+    elementList.append(s1_lc1)
+    s1_rc1 = c.getRightChild(s1,1)
+    elementList.append(s1_rc1)
+    s1_lc2 = c.getLeftChild(s1,2)
+    elementList.append(s1_lc2)
+    s1_rc2 = c.getRightChild(s1,2)
+    elementList.append(s1_rc2)
+
+    # s2 data
+    s2_lc1 = c.getLeftChild(s2,1)
+    elementList.append(s2_lc1)
+    s2_rc1 = c.getRightChild(s2,1)
+    elementList.append(s2_rc1)
+    s2_lc2 = c.getLeftChild(s2,2)
+    elementList.append(s2_lc2)
+    s2_rc2 = c.getRightChild(s2,2)
+    elementList.append(s2_rc2)
+
+    # leftmost/rightmost of rightmost children
+    s1_lc1_lc1 = c.getLeftChild(s1_lc1,1)
+    elementList.append(s1_lc1_lc1)
+    s1_rc1_rc1 = c.getRightChild(s1_rc1,1)
+    elementList.append(s1_rc1_rc1)
+
+    s2_lc1_lc1 = c.getLeftChild (s2_lc1,1)
+    elementList.append(s2_lc1_lc1)
+    s2_rc1_rc1 = c.getRightChild (s2_rc1,1)
+    elementList.append(s2_rc1_rc1)
+
+
+    wordIDs = []
+    labelIDs = []
+    tagIDs  = []
+
+    print elementList
+
+    for i in elementList:
+        wordIDs.append(getWordID(c.getWord(i)))
+
+    for i in elementList:
+        tagIDs.append(getPosID(c.getPOS(i)))
+
+
+    for i in elementList[6:]:
+	print "element: ",i
+        labelIDs.append(getLabelID(c.getLabel(i)))
+
+    mergedList = wordIDs + tagIDs + labelIDs
+    # Method 1
+    return mergedList
+
+    
+    """
+    #Method 2
+    row = 48, col = 50;
+    j=0
+    Matrix = [[0 for x in range(col)] for y in range(row)]
+
+    for x in range (row):
+        Matrix[x] = embedding_array[mergedList[j]]
+        j+=1
+
+    return Matrix
+    """
+
+    """
+    #Words of the selected elements
+    s1Word = c.getWord(s1)
+    addElement(elementList,getWordID(s1Word),i)
+    i+=1  
+    s2Word = c.getWord(s2)
+    addElement(elementList,getWordID(s2Word),i)
+    i+=1
+    s3Word = c.getWord(s3)
+    addElement(elementList,getWordID(s3Word),i)
+    i+=1
+
+    b1Word = c.getWord(b1)
+    addElement(elementList,getWordID(b1Word),i)
+    i+=1
+    b2Word = c.getWord(b2)
+    addElement(elementList,getWordID(b2Word),i)
+    i+=1
+    b3Word = c.getWord(b3)
+    addElement(elementList,getWordID(b3Word),i)
+    i+=1
+
+    s1_lc1Word = c.getWord(s1_lc1)
+    addElement(elementList,getWordID(s1_lc1Word),i)
+    i+=1
+    s1_rc1Word = c.getWord(s1_rc1)
+    addElement(elementList,getWordID(s1_rc1Word),i)
+    i+=1
+    s1_lc2Word = c.getWord(s1_lc2)
+    addElement(elementList,getWordID(s1_lc2Word),i)
+    i+=1
+    s1_rc2Word = c.getWord(s1_rc2)
+    addElement(elementList,getWordID(s1_rc2Word),i)
+    i+=1
+
+    s2_lc1Word = c.getWord(s2_lc1)
+    addElement(elementList,getWordID(s2_lc1Word),i)
+    i+=1
+    s2_rc1Word = c.getWord(s2_rc1)
+    addElement(elementList,getWordID(s2_rc1Word),i)
+    i+=1
+    s2_lc2Word = c.getWord(s2_lc2)
+    addElement(elementList,getWordID(s2_lc2Word),i)
+    i+=1
+    s2_rc2Word = c.getWord(s2_rc2)
+    addElement(elementList,getWordID(s2_rc2Word),i)
+    i+=1
+
+    s1_lc1_lc1Word = c.getWord(s1_lc1_lc1)
+    addElement(elementList,getWordID(s1_lc1_lc1Word),i)
+    i+=1
+    s1_rc1_rc1Word = c.getWord(s1_rc1_rc1)
+    addElement(elementList,getWordID(s1_rc1_rc1Word),i)
+    i+=1
+    s2_lc1_lc1Word = c.getWord(s2_lc1_lc1)
+    addElement(elementList,getWordID(s2_lc1_lc1Word),i)
+    i+=1
+    s2_rc1_rc1Word = c.getWord(s2_rc1_rc1)
+    addElement(elementList,getWordID(s2_rc1_rc1Word),i)
+    i+=1
+
+
+    #POS tags of the selected elements
+    s1POS = c.getPOS(s1)
+    addElement(elementList,getPosID(s1POS),i)
+    i+=1
+    s2POS = c.getPOS(s2)
+    addElement(elementList,getPosID(s2POS),i)
+    i+=1
+    s3POS = c.getPOS(s3)
+    addElement(elementList,getPosID(s3POS),i)
+    i+=1
+
+    b1POS = c.getPOS(b1)
+    addElement(elementList,getPosID(b1POS),i)
+    i+=1
+    b2POS = c.getPOS(b2)
+    addElement(elementList,getPosID(b2POS),i)
+    i+=1
+    b3POS = c.getPOS(b3)
+    addElement(elementList,getPosID(b3POS),i)
+    i+=1
+
+    s1_lc1POS = c.getPOS(s1_lc1)
+    addElement(elementList,getPosID(s1_lc1POS),i)
+    i+=1
+    s1_rc1POS = c.getPOS(s1_rc1)
+    addElement(elementList,getPosID(s1_rc1POS),i)
+    i+=1
+    s1_lc2POS = c.getPOS(s1_lc2)
+    addElement(elementList,getPosID(s1_lc2POS),i)
+    i+=1
+    s1_rc2POS = c.getPOS(s1_rc2)
+    addElement(elementList,getPosID(s1_rc2POS),i)
+    i+=1
+
+    s2_lc1POS = c.getPOS(s2_lc1)
+    addElement(elementList,getPosID(s2_lc1POS),i)
+    i+=1
+    s2_rc1POS = c.getPOS(s2_rc1)
+    addElement(elementList,getPosID(s2_rc1POS),i)
+    i+=1
+    s2_lc2POS = c.getPOS(s2_lc2)
+    addElement(elementList,getPosID(s2_lc2POS),i)
+    i+=1
+    s2_rc2POS = c.getPOS(s2_rc2)
+    addElement(elementList,getPosID(s2_rc2POS),i)
+    i+=1
+
+    s1_lc1_lc1POS = c.getPOS(s1_lc1_lc1)
+    addElement(elementList,getPosID(s1_lc1_lc1POS),i)
+    i+=1
+    s1_rc1_rc1POS = c.getPOS(s1_rc1_rc1)
+    addElement(elementList,getPosID(s1_rc1_rc1POS),i)
+    i+=1
+    s2_lc1_lc1POS = c.getPOS(s2_lc1_lc1)
+    addElement(elementList,getPosID(s2_lc1_lc1POS),i)
+    i+=1
+    s2_rc1_rc1POS = c.getPOS(s2_rc1_rc1)
+    addElement(elementList,getPosID(s2_rc1_rc1POS),i)
+    i+=1
+    
+
+
+    #Labels of the selected elements
+    s1_lc1Label = c.getLabel(s1_lc1)
+    addElement(elementList,getLabelID(s1_lc1Label)s,i)
+    i+=1
+    s1_rc1Label = c.getLabel(s1_rc1)
+    addElement(elementList,getLabelID(s1_rc1Label),i)
+    i+=1
+    s1_lc2Label = c.getLabel(s1_lc2)
+    addElement(elementList,getLabelID(s1_lc2Label),i)
+    i+=1
+    s1_rc2Label = c.getLabel(s1_rc2)
+    addElement(elementList,getLabelID(s1_rc2Label),i)
+    i+=1
+
+    s2_lc1Label = c.getLabel(s2_lc1)
+    addElement(elementList,getLabelID(s2_lc1Label),i)
+    i+=1
+    s2_rc1Label = c.getLabel(s2_rc1)
+    addElement(elementList,getLabelID(s2_rc1Label),i)
+    i+=1
+    s2_lc2Label = c.getLabel(s2_lc2)
+    addElement(elementList,getLabelID(s2_lc2Label),i)
+    i+=1
+    s2_rc2Label = c.getLabel(s2_rc2)
+    addElement(elementList,getLabelID(s2_rc2Label),i)
+    i+=1
+
+    s1_lc1_lc1Label = c.getLabel(s1_lc1_lc1)
+    addElement(elementList,getLabelID(s1_lc1_lc1Label),i)
+    i+=1
+    s1_rc1_rc1Label = c.getLabel(s1_rc1_rc1)
+    addElement(elementList,getLabelID(s1_rc1_rc1Label),i)
+    i+=1
+    s2_lc1_lc1Label = c.getLabel(s2_lc1_lc1)
+    addElement(elementList,getLabelID(s2_lc1_lc1Label),i)
+    i+=1
+    s2_rc1_rc1Label = c.getLabel(s2_rc1_rc1)
+    addElement(elementList,getLabelID(s2_rc1_rc1Label),i)
+    i+=1
+    """
+    """
+    row = 48, col = 50;
+    j=0
+    Matrix = [[0 for x in range(col)] for y in range(row)]
+
+    for x in range (row):
+        Matrix[x] = elementList[j]
+        j+=1 
+    """
+    """
+    return elementList
+    """
+
+
+def genTrainExamples(sents, trees):
+    numTrans = parsing_system.numTransitions()
+
+    features = []
+    labels = []
+    pbar = ProgressBar()
+    for i in pbar(range(len(sents))):
+        if trees[i].isProjective():
+            c = parsing_system.initialConfiguration(sents[i])
+
+            while not parsing_system.isTerminal(c):
+                oracle = parsing_system.getOracle(c, trees[i])
+                feat = getFeatures(c)
+                label = []
+                for j in range(numTrans):
+                    t = parsing_system.transitions[j]
+                    if t == oracle:
+                        label.append(1.)
+                    elif parsing_system.canApply(c, t):
+                        label.append(0.)
+                    else:
+                        label.append(-1.)
+
+                if 1.0 not in label:
+                    print i, label
+                features.append(feat)
+                labels.append(label)
+                c = parsing_system.apply(c, oracle)
+    return features, labels
+
+
+def load_embeddings(filename, wordDict, posDict, labelDict):
+    dictionary, word_embeds = pickle.load(open(filename, 'rb'))
+
+    embedding_array = np.zeros((len(wordDict) + len(posDict) + len(labelDict), Config.embedding_size))
+    knownWords = wordDict.keys()
+    foundEmbed = 0
+    for i in range(len(embedding_array)):
+        index = -1
+        if i < len(knownWords):
+            w = knownWords[i]
+            if w in dictionary:
+                index = dictionary[w]
+            elif w.lower() in dictionary:
+                index = dictionary[w.lower()]
+        if index >= 0:
+            foundEmbed += 1
+            embedding_array[i] = word_embeds[index]
+        else:
+            embedding_array[i] = np.random.rand(Config.embedding_size) * 0.02 - 0.01
+    print "Found embeddings: ", foundEmbed, "/", len(knownWords)
+
+    return embedding_array
+
+
+if __name__ == '__main__':
+
+    wordDict = {}
+    posDict = {}
+    labelDict = {}
+    parsing_system = None
+
+    trainSents, trainTrees = Util.loadConll('train.conll')
+    devSents, devTrees = Util.loadConll('dev.conll')
+    testSents, _ = Util.loadConll('test.conll')
+    genDictionaries(trainSents, trainTrees)
+
+    embedding_filename = 'word2vec.model'
+
+    embedding_array = load_embeddings(embedding_filename, wordDict, posDict, labelDict)
+
+    labelInfo = []
+    for idx in np.argsort(labelDict.values()):
+        labelInfo.append(labelDict.keys()[idx])
+    parsing_system = ParsingSystem(labelInfo[1:])
+    print parsing_system.rootLabel
+
+    print "Generating Traning Examples"
+    trainFeats, trainLabels = genTrainExamples(trainSents, trainTrees)
+    print "Done."
+
+    # Build the graph model
+    graph = tf.Graph()
+    model = DependencyParserModel(graph, embedding_array, Config)
+
+    num_steps = Config.max_iter
+    with tf.Session(graph=graph) as sess:
+
+        model.train(sess, num_steps)
+
+        model.evaluate(sess, testSents)
+
---
+++ ParsingSystem.py
@@ -0,0 +1,266 @@
+from Configuration import Configuration
+import Config
+
+"""
+Defines a transition-based parsing framework for dependency parsing
+based on an arc-standard transition-based dependency parsing system (Nivre, 2004).
+
+Author: Danqi Chen
+Modified by: Heeyoung Kwon
+"""
+
+class ParsingSystem:
+    def __init__(self, labels):
+        self.singleRoot = True
+        self.labels = labels
+        self.transitions = []
+        self.rootLabel = labels[0]
+        self.makeTransitions()
+
+    """
+    Generate all possible transitions which this parsing system can
+    take for any given configuration.
+    """
+    def makeTransitions(self):
+        for label in self.labels:
+            self.transitions.append("L(" + label + ")")
+        for label in self.labels:
+            self.transitions.append("R(" + label + ")")
+
+        self.transitions.append("S")
+
+    def initialConfiguration(self, s):
+        c = Configuration(s)
+        length = len(s)
+
+        # For each token, add dummy elements to the configuration's tree
+        # and add the words onto the buffer
+        for i in range(1, length+1):
+            c.tree.add(Config.NONEXIST, Config.UNKNOWN)
+            c.buffer.append(i)
+
+        # Put the ROOT node on the stack
+        c.stack.append(0)
+
+        return c
+
+    def isTerminal(self, c):
+        return c.getStackSize() == 1 and c.getBufferSize() == 0
+
+    """
+    Provide a static-oracle recommendation for the next parsing step to take
+    """
+    def getOracle(self, c, tree):
+        w1 = c.getStack(1)
+        w2 = c.getStack(0)
+        if w1 > 0 and tree.getHead(w1) == w2:
+            return "L(" + tree.getLabel(w1) + ")"
+        elif w1 >= 0 and tree.getHead(w2) == w1 and not c.hasOtherChild(w2, tree):
+            return "R(" + tree.getLabel(w2) + ")"
+        else:
+            return "S"
+
+    """
+    Determine whether the given transition is legal for this
+    configuration.
+    """
+    def canApply(self, c, t):
+        if t.startswith("L") or t.startswith("R"):
+            label = t[2:-1]
+            if t.startswith("L"):
+                h = c.getStack(0)
+            else:
+                h = c.getStack(1)
+            if h < 0:
+                return False
+            if h == 0 and label != self.rootLabel:
+                return False
+
+        nStack = c.getStackSize()
+        nBuffer = c.getBufferSize()
+
+        if t.startswith("L"):
+            return nStack > 2
+        elif t.startswith("R"):
+            if self.singleRoot:
+                return (nStack > 2) or (nStack == 2 and nBuffer == 0)
+            else:
+                return nStack >= 2
+        else:
+            return nBuffer > 0
+
+    def apply(self, c, t):
+
+        """
+        =================================================================
+
+        Implement arc standard algorithm based on
+        Incrementality in Deterministic Dependency Parsing(Nirve, 2004):
+        Left-reduce
+        Right-reduce
+        Shift
+
+        =================================================================
+        """
+        if self.canApply(c,t):
+
+	        if t.startswith("L"):
+        		se = t.index('(')
+        		ee = t.index(')')
+	        	#Apply Left arc from paper
+	        	w_j = c.getStack(1)
+		        w_i = c.getStack(0)
+
+		        #add left arc
+		        #head word should be w_i
+		        # We will have to get these labels
+		        c.addArc(w_j,w_i,t[se+1:ee])
+
+
+		        #Keep only w_j on stack
+		        #w_i=c.stack.pop();
+		        c.stack.pop();
+		        #c.stack.push(w_i);
+
+
+	        elif t.startswith("R"):
+        		se = t.index('(')
+        		ee = t.index(')')
+	        	#Apply Right arc from paper
+	        	w_j = c.getStack(1)
+		        w_i = c.getStack(0)
+
+				#add right arc
+		        c.addArc(w_i,w_j,t[se+1:ee])
+
+		        #Keep only w_j on stack
+		        t = c.stack.pop();
+		        c.stack.pop();
+		        c.stack.append(t)
+
+
+	        elif t.startswith("S"):
+	        	#Apply Shift from paper
+	        	c.shift()
+	 #	else:
+	 #		print ("illegal transaction")
+
+	return c
+
+
+        """
+        flag = true
+        while flag:
+        	flag = false
+	        s = getStack(0)
+	        b = getBuffer(0)
+
+	        #Left arc
+	        #havent handled the condition to check if (b,s,*) already in op
+	        if getHead(s) == b:
+	        	print("The word ", b ," is head of ",s)
+	        	addArc(b,s,"Left")
+	        	#addArc(,,"left")
+
+	        #Right arc:
+	    	elif getHead(b) in self.stack:
+	        	addArc(i,b,"Right")
+	        else:
+	        	#Shift
+		"""
+
+
+
+
+
+
+
+
+    def numTransitions(self):
+        return len(self.transitions)
+
+    def printTransitions(self):
+        for t in self.transitions:
+            print t
+
+    def getPunctuationTags(self):
+        return ["''", ",", ".", ":", "``", "-LRB-", "-RRB-"]
+
+    """
+    Evaluate performance on a list of sentences, predicted parses, and gold parses
+    """
+    def evaluate(self, sentences, trees, goldTrees):
+        result = []
+        punctuationTags = self.getPunctuationTags()
+
+        if len(trees) != len(goldTrees):
+            print "Incorrect number of trees."
+            return None
+
+        correctArcs = 0
+        correctArcsNoPunc = 0
+        correctHeads = 0
+        correctHeadsNoPunc = 0
+
+        correctTrees = 0
+        correctTreesNoPunc = 0
+        correctRoot = 0
+
+        sumArcs = 0
+        sumArcsNoPunc = 0
+
+        for i in range(len(trees)):
+            tree = trees[i]
+            goldTree = goldTrees[i]
+            tokens = sentences[i]
+
+            if tree.n != goldTree.n:
+                print "Tree", i+1, ": incorrect number of nodes."
+                return None
+
+            if not tree.isTree():
+                print "Tree", i+1, ": illegal."
+                return None
+
+            nCorrectHead = 0
+            nCorrectHeadNoPunc = 0
+            nNoPunc = 0
+
+            for j in range(1, tree.n+1):
+                if tree.getHead(j) == goldTree.getHead(j):
+                    correctHeads += 1
+                    nCorrectHead += 1
+                    if tree.getLabel(j) == goldTree.getLabel(j):
+                        correctArcs += 1
+                sumArcs += 1
+
+                tag = tokens[j-1]['POS']
+                if tag not in punctuationTags:
+                    sumArcsNoPunc += 1
+                    nNoPunc += 1
+                    if tree.getHead(j) == goldTree.getHead(j):
+                        correctHeadsNoPunc += 1
+                        nCorrectHeadNoPunc += 1
+                        if tree.getLabel(j) == goldTree.getLabel(j):
+                            correctArcsNoPunc += 1
+
+            if nCorrectHead == tree.n:
+                correctTrees += 1
+            if nCorrectHeadNoPunc == nNoPunc:
+                correctTreesNoPunc += 1
+            if tree.getRoot() == goldTree.getRoot():
+                correctRoot += 1
+
+        result = ""
+        result += "UAS: " + str(correctHeads * 100.0 / sumArcs) + "\n"
+        result += "UASnoPunc: " + str(correctHeadsNoPunc * 100.0 / sumArcsNoPunc) + "\n"
+        result += "LAS: " + str(correctArcs * 100.0 / sumArcs) + "\n"
+        result += "LASnoPunc: " + str(correctArcsNoPunc * 100.0 / sumArcsNoPunc) + "\n\n"
+
+        result += "UEM: " + str(correctTrees * 100.0 / len(trees)) + "\n"
+        result += "UEMnoPunc: " + str(correctTreesNoPunc * 100.0 / len(trees)) + "\n"
+        result += "ROOT: " + str(correctRoot * 100.0 / len(trees)) + "\n"
+
+        return result
+
+print "You are in Dependency parser"
